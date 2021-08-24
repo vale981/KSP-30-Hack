8056e2858987b52c3af8c8860d80efb12ed2ffa6
Valentin Boettcher <hiro@protagon.space>
2021-08-24 12:45:03 +0200

    port to pro micro

--- keyboard/keyboard.ino
+++ keyboard/keyboard.ino
@@ -2,34 +2,7 @@
 /*                                    Pins                                   */
 /*****************************************************************************/
 
-// columns
-constexpr int KD1 = 14;
-constexpr int KD2 = 15;
-constexpr int KD3 = 16;
-constexpr int KD4 = 17;
-constexpr int KD5 = 18;
-constexpr int KD6 = 19;
-constexpr int KD7 = 2;
-constexpr int KD8 = 3;
-
-// row selectors
-constexpr int SA1 = 4;
-constexpr int SA2 = 5;
-constexpr int SA3 = 6;
-
-constexpr int SEL_A = SA1;
-constexpr int SEL_B = SA2;
-constexpr int SEL_C = SA3;
-
-// range selector
-constexpr int SA4 = 7;
-constexpr int SA5 = 8;
-
-constexpr int RS1 = SA4;
-constexpr int RS2 = SA5;
-
-// sustain
-constexpr int SSTE = 9;
+#include "./pro_micro.h"
 
 /*****************************************************************************/
 /*                                Convenience                                */
---
+++ keyboard/pro_micro.h
@@ -0,0 +1,32 @@
+/*****************************************************************************/
+/*                                    Pins                                   */
+/*****************************************************************************/
+
+// columns
+constexpr int KD1 = 0;
+constexpr int KD2 = 1;
+constexpr int KD3 = 2;
+constexpr int KD4 = 3;
+constexpr int KD5 = 4;
+constexpr int KD6 = 5;
+constexpr int KD7 = 6;
+constexpr int KD8 = 7;
+
+// row selectors
+constexpr int SA1 = 8;
+constexpr int SA2 = 9;
+constexpr int SA3 = 14;
+
+constexpr int SEL_A = SA1;
+constexpr int SEL_B = SA2;
+constexpr int SEL_C = SA3;
+
+// range selector
+constexpr int SA4 = 15;
+constexpr int SA5 = 16;
+
+constexpr int RS1 = SA4;
+constexpr int RS2 = SA5;
+
+// sustain
+constexpr int SSTE = 21;
